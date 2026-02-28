38214055ee87c145681f4cee1e8e067c877c9a93
sandstranger <sandstranger@gmail.com>
2026-02-28 21:55:46 +0400

    fix lua crash on Android

--- src/xrScriptEngine/script_engine.cpp
+++ src/xrScriptEngine/script_engine.cpp
@@ -801,13 +801,9 @@ void CScriptEngine::init(export_func exporter, bool loadGlobalNamespace)
         luabind::allow_nil_conversion(nilConversion);
         luabind::disable_super_deprecation();
 
-#ifndef ANDROID
         const bool escapeSequences =
             pSettingsOpenXRay->read_if_exists<bool>("lua_scripting", "allow_escape_sequences", false);
         luajit::allow_escape_sequences(escapeSequences);
-#else
-        luajit::allow_escape_sequences(true);
-#endif
     }
 
     luabind::bind_class_info(lua());
