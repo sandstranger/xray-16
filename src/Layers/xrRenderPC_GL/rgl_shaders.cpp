1a88e74e021bd8eb9ae689c48b0a4d8e86b88778
sandstranger <sandstranger@gmail.com>
2026-03-14 01:57:57 +0400

    better soft particles disabling for Android

--- src/Layers/xrRenderPC_GL/rgl_shaders.cpp
+++ src/Layers/xrRenderPC_GL/rgl_shaders.cpp
@@ -360,13 +360,15 @@ HRESULT CRender::shader_compile(pcstr name, IReader* fs, pcstr pFunctionName,
         sh_name.append(static_cast<u32>(0));
     }
 
-#ifndef ANDROID
     // Soft particles
     {
+#ifndef ANDROID
         const bool useSoftParticles = RImplementation.o.advancedpp && ps_r2_ls_flags.test(R2FLAG_SOFT_PARTICLES);
+#else
+        const bool useSoftParticles = false;
+#endif
         appendShaderOption(useSoftParticles, "USE_SOFT_PARTICLES", "1");
     }
-#endif
 
     // Depth of field
     {
